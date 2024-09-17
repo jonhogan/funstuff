5595d79c34d151839addae9594cee258142b90ec
Jon H <jon.hogan83@gmail.com>
2024-09-17 17:48:01 -0500

    Added loop w/exit

--- name_gen/main.py
+++ name_gen/main.py
@@ -4,8 +4,21 @@ fName = ("Baby Oil", "Bad News", "Big Burps", "Beenie-Weenie",
          "Lady Bug", "Jumbo Jim", "Little John", "Slurmy Slurm", "Schlomo", "Red Five", "Gold Leader")
 lName = ("Appleyard", "Jones", "Jefferson", "Mugglesworth","Clovenhoof", "Porkins", "Pennywise")
 
+repeat = True
+continueAnswer =" "
 
-firstName = random.choice(fName)
-lastName = random.choice(lName)
+while(repeat):
 
-print(firstName + " " + lastName)
+    firstName = random.choice(fName)
+    lastName = random.choice(lName)
+
+    print(firstName + " " + lastName)
+
+    continueAnswer = input("Would you like a new name? ").lower()
+
+    if continueAnswer in ["no", "n"]:
+        repeat = False
+
+
+
+
