911818c8398ecce4a867348ed58e641f5c2a6a1e
Jon H <jon.hogan83@gmail.com>
2024-09-18 21:03:50 -0500

    Updated var names

--- name_gen/main.py
+++ name_gen/main.py
@@ -1,27 +1,27 @@
-import sys, random
-from colorama import init as colorama_init
+"""Generate silly names chosen at random from two lists"""
+
+import random
 from colorama import Fore
 from colorama import Style
 
+
+
 fName = ("Baby Oil", "Bad News", "Big Burps", "Beenie-Weenie",
-         "Lady Bug", "Jumbo Jim", "Little John", "Slurmy Slurm", "Schlomo", "Red Five", "Gold Leader")
+         "Lady Bug", "Jumbo Jim", "Little John", "Slurmy Slurm",
+           "Schlomo", "Red Five", "Gold Leader")
 lName = ("Appleyard", "Jones", "Jefferson", "Mugglesworth","Clovenhoof", "Porkins", "Pennywise")
 
 repeat = True
-continueAnswer =" "
+cAnswer =" "
 
-while(repeat):
+while repeat:
 
     firstName = random.choice(fName)
     lastName = random.choice(lName)
 
     print(f"{Fore.RED}{firstName} {lastName}{Style.RESET_ALL}")
 
-    continueAnswer = input("Would you like a new name? ").lower()
+    cAnswer = input("Would you like a new name? ").lower()
 
-    if continueAnswer in ["no", "n"]:
+    if cAnswer in ["no", "n"]:
         repeat = False
-
-
-
-
