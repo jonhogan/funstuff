bc63fa22a0950c089d9590a24f7967aa5d772bf7
Jon H <jon.hogan83@gmail.com>
2024-09-19 20:20:23 -0500

    Initial push

---
+++ coding_exercise/exercise5/main.py
@@ -0,0 +1,28 @@
+"""Bug-Fixing Exercise 1
+
+The code below tries to write the items of temperatures each in one line in the file.txt list. However, the code has an error. Try to fix the error.
+
+    temperatures = [10, 12, 14]
+     
+    file = open("file.txt", 'w')
+     
+    file.writelines(temperatures)
+"""
+
+temperatures = [10, 12, 14]
+temperatures = [str(i) + '/n' for i in temperatures]     
+file = open("file.txt", 'w')
+file.writelines(temperatures)
+
+
+"""Bug-Fixing Exercise 2
+
+The code below tries to convert all the numbers to integers. However, the code has an error. Try to fix the error.
+
+    numbers = [10.1, 12.3, 14.7]
+    numbers = [int(number) for item in numbers]
+    print(numbers)"""
+
+numbers = [10.1, 12.3, 14.7]
+numbers = [int(number) for number in numbers]
+print(numbers)
