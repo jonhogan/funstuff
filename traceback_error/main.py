2df5f656fd9fdbb060422edd5d08cd7ac5cf298a
Jon H <jon.hogan83@gmail.com>
2024-09-21 17:21:52 -0500

    Allowed user input

--- traceback_error/main.py
+++ traceback_error/main.py
@@ -1,23 +1,28 @@
 '''Simple program with errors that need to be handled'''
 
-def function_a():
+def function_a(x, y):
     print("Start a()")
     #Call b()
-    function_b()
+    function_b(x, y)
 
-def function_b():
+def function_b(x, y):
     print("Start b()")
     #Call c()
-    function_c()
+    function_c(x, y)
 
-def function_c():
+def function_c(x, y):
     print("Start c()")
     #Code with error: 42/0
     try:
-        42 / 0
+        x / y
     except ZeroDivisionError:
         print("Cannot divide by zero!")
+    else:
+        print(f"{x} divided by {y} equals {x/y}")
 
     print("Program has successfully completed.")
 
-function_a()
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+             
+function_a(num1, num2)
